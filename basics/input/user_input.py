d9e15988923180670fc1819b2688214713d4eb51
repl.it user <replituser@example.com>
2020-10-04 21:30:29 +0000

    Added code to read user input.

---
+++ basics/input/user_input.py
@@ -0,0 +1,17 @@
+# Ask user to enter their name
+# Input example 1
+print("What is your name human?")
+name = input()
+print("It is nice to meet you human", name)
+print("Nice to meet you human", name, ".")
+print("Nice to meet you" + name + ".")
+print("Nice to meet you " + name + ".")
+
+
+# Input example 2
+# Read in user's surname
+surname = input("\nWhat is your surname human?\n")
+print("\nIt is nice to meet you human", surname)
+print("Nice to meet you human", surname, ".")
+print("Nice to meet you" + surname + ".")
+print("Nice to meet you " + surname + ".")
