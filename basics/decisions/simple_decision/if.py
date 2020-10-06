b6d11e925dc8702fa67f303aedd2b6d730c86e95
repl.it user <replituser@example.com>
2020-10-06 09:15:00 +0000

    Added code to demonstrate the use of an if statement.

---
+++ basics/decisions/simple_decision/if.py
@@ -0,0 +1,21 @@
+# check whether first number is bigger than the second number.
+# Assume first_number and second_number are existing variables.
+#print("please enter first number")
+#first_number = input()
+
+#print("please enter second number")
+#second_number = input()
+
+#if (first_number > second_number):
+  #print("First is bigger!")
+
+#print("Done!")
+
+print("What type of book is this?")
+book_name = input()
+adventure = "adventure"
+
+if (book_name == adventure):
+  print("I like adventure books!")
+
+print("Finished reading book.")
