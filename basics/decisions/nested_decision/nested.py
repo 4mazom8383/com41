70fafa1b91440f345dbef41a1242856565e4856d
repl.it user <replituser@example.com>
2020-10-09 10:39:44 +0000

    Added code to demonstrate the use of a nested decision statement.

--- basics/decisions/nested_decision/nested.py
+++ basics/decisions/nested_decision/nested.py
@@ -5,14 +5,35 @@ activity = input()
 # decide if beep should play or study
 if (activity == "play"):
 
-    # ask user what to play with
-    print("What should I play with (toy/friend)?")
-    play_with = input()
-    
-    # decide if beep should play with toys or friend
-    if (play_with == "toy"):
-        print("I will play with my toys!")
-    else:
-        print("I will play with my friend!")
+  # ask user what to play with
+  print("What should I play with (toy/friend)?")
+  play_with = input()
+  
+  # decide if beep should play with toys or friend
+  if (play_with == "toy"):
+    print("I will play with my toys!")
+  else:
+    print("I will play with my friend!")
+
+
 else:
-    print("I will study")
+  print("I will study")
+
+
+
+#Code Two
+print("\nWhat type of cover does the book have?")
+book = input()
+
+if(book == "soft"):
+  print("Is the book perfect-bound?")
+  perfect_bound = input()
+
+  if(perfect_bound == "yes"):
+    print("Soft cover, perfect bound books are very popular!")
+  else:
+    print("Soft covers with coils or stitches are great for short books")
+
+elif (book == "hard"):
+  print("Books with hard covers can be more expensive!")
+
