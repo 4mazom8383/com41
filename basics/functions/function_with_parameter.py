dde93edfaa4752cafbd1a69f8520e819d4f1c8e3
repl.it user <replituser@example.com>
2020-10-15 18:30:50 +0000

    Added code to demonstrate the use of a user-defined function with multiple parameters.

--- basics/functions/function_with_parameter.py
+++ basics/functions/function_with_parameter.py
@@ -1,7 +1,4 @@
-def escape_by(plan=''):
-
-  print("Entre your plan")
-  plan = input()
+def escape_by(plan):
 
   if(plan == "jumping over"):
     print("We cannot escape that way! The boulder is too big!")
@@ -13,4 +10,6 @@ def escape_by(plan=''):
     print("We cannot escape that way! The boulder is in the way!")
 
 # Calling function
-escape_by()
+escape_by("jumping over")
+escape_by("running around")
+escape_by("going deeper") 
