3eb4530677f321e785f2af68b9907fbc01a3ff72
repl.it user <replituser@example.com>
2020-10-20 09:28:41 +0000

    Added code to demonstrate a simple list.

---
+++ data/lists/simple_list.py
@@ -0,0 +1,19 @@
+#fruits = []
+
+#fruits = ["apple", "banana", "cherry"]
+
+#fruits.append("dragon fruit")
+
+#fruits.remove("dragon fruit")
+
+#print(fruits)
+
+
+def directions():
+  directions = ["Move Forward","Move Backward", "Turn Left", "Turn Right"]
+  return directions
+
+def run():
+  print(directions())
+
+run()
