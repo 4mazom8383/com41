b42d168ea3cc7720038863cc1ae56e366b506cf7
repl.it user <replituser@example.com>
2020-10-23 09:43:38 +0000

    Added code to demonstrate how to return a tuple from a function.

---
+++ data/tuples/function_tuple.py
@@ -0,0 +1,13 @@
+
+
+
+def likelihood():
+  likelihoods = (50, 38, 27, 99, 4)
+  return likelihoods
+
+def run():
+  run = likelihood()
+  print("Minimum likelihood of falling: {} %".format(min(run)))
+  print("Minimum likelihood of falling: {} %".format(max(run)))
+  
+run()
