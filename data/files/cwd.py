306492dfdcb27db1c5743717a9afa5fa7cae5627
repl.it user <replituser@example.com>
2020-11-03 10:24:22 +0000

    Added code to display cwd information.

---
+++ data/files/cwd.py
@@ -0,0 +1,24 @@
+#Find the folder path
+import os
+path = os.getcwd()
+print(f"Current Working Folder Path: {path}")
+
+#Folder content
+for file in os.listdir(path):
+  print(file)
+
+print("")
+print("")
+print("")
+
+def cwd():
+  
+  path = os.getcwd()
+  print(f"Current Working Folder Path: {path}")
+  print("The folder contains the following:")
+
+def run():
+  print("Processing...")
+  cwd()
+
+run()
