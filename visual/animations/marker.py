cb542a36ecc7200cbe05b85b54cd74aecf953566
Mohamed Mazouz <4mazom83@solent.ac.uk>
2020-11-24 10:20:24 +0000

    Added code to display an animated marker.

--- visual/animations/marker.py
+++ visual/animations/marker.py
@@ -5,15 +5,15 @@ fig, ax = plt.subplots()
     
 def animate(frame):    
   # your code here
-  #Cla.() #Cla.() shows only the current position of our marker.
-  #ax.cla()
-  ax.set_xlim(0,20)
-  ax.set_ylim(0,20)
+  #Cla.() shows only the current position of our marker.
+  ax.cla()
+  ax.set_xlim(0,10)
+  ax.set_ylim(0,10)
   ax.plot(frame,frame, 'ro')
      
 def run():
   #global fig  
-  simple_animation = animation.FuncAnimation(fig, animate, frames = 20, interval = 1000, repeat = False)
+  simple_animation = animation.FuncAnimation(fig, animate, frames = 10, interval = 1000, repeat = False)
 
   plt.show()
       
