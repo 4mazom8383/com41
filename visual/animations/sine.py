ff6b2f08d3de4d1840bacf2327274be18203472e
Mohamed Mazouz <4mazom83@solent.ac.uk>
2020-11-26 11:53:18 +0000

    Added code to visualise a sine wave.

---
+++ visual/animations/sine.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt   
+import matplotlib.animation as animation
+import numpy as np
+
+fig, ax = plt.subplots()
+
+def animate(frame_number):
+  global ax
+  ax.set_xlim(0,720)
+  ax.set_ylim(-1,1)
+
+  #NumPy array named x with a range from 0 to the current frame number.
+  x_in_degrees = np.arange(0, frame_number)
+
+  #Calculate the y values by multiplying the x values by PI/180 and then find the sine of the result (i.e. sine of (x * PI/180)).
+  x_in_radians = x_in_degrees * (np.pi / 180)
+
+  # Store the y value on Y varible
+  y = np.sin(x_in_radians)
+
+  # plot the points given be x and y.
+  ax.plot(x_in_degrees, y)
+
+
+
+def run():
+
+
+  global line
+  sine_animation = animation.FuncAnimation( 
+    fig, 
+    animate, 
+    frames = 720, 
+    interval = 100
+  )
+
+
+run()
+
